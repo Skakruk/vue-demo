377913c889336bbfac9d84719abc600a7339520e
Viktor Kenyz <viktor.kenyz@thetileapp.com>
2018-03-02 14:40:57 +0200

    Fix API

--- src/components/TodoPage.vue
+++ src/components/TodoPage.vue
@@ -23,151 +23,149 @@
 </template>
 
 <script>
-import axios from 'axios';
+  import axios from 'axios';
 
-import AddTaskForm from './AddTaskForm';
-import FormFooter from './FormFooter';
-import TaskList from './TaskList';
+  import AddTaskForm from './AddTaskForm';
+  import FormFooter from './FormFooter';
+  import TaskList from './TaskList';
 
-const API_ENDPOINT = 'https://floating-fjord-98973.herokuapp.com/todo';
+  const API_ENDPOINT = 'https://floating-fjord-98973.herokuapp.com/todo';
+  // const API_ENDPOINT = 'http://localhost:8888/todo';
 
-export default {
-  name: 'todo-page',
-  props: ['status'],
-  components: {
-    AddTaskForm,
-    FormFooter,
-    TaskList,
-  },
-  data() {
-    return {
-      items: [],
-    };
-  },
-  created() {
-    axios.get(API_ENDPOINT)
-      .then((response) => {
-        this.items = response.data;
-      })
-      .catch((e) => {
-        this.errors.push(e);
-      });
-  },
-  computed: {
-    // a computed getter
-    completedCount() {
-      // `this` points to the vm instance
-      return this.items.filter(item => !item.isCompleted).length;
+  export default {
+    name: 'todo-page',
+    props: ['status'],
+    components: {
+      AddTaskForm,
+      FormFooter,
+      TaskList,
     },
-    total() {
-      return this.items.length;
+    data() {
+      return {
+        items: [],
+      };
     },
-    filteredItems() {
-      return this.items.filter((item) => {
-        switch (this.status) {
-          case 'active':
-            return !item.isCompleted;
-          case 'completed':
-            return item.isCompleted;
-          default:
-            return true;
-        }
-      });
-    },
-  },
-  methods: {
-    addItem(task) {
-      axios.post(API_ENDPOINT, {
-        task
-      })
+    created() {
+      axios.get(API_ENDPOINT, { withCredentials: true })
         .then((response) => {
-          this.items.push(response.data);
+          this.items = response.data;
         })
         .catch((e) => {
           this.errors.push(e);
         });
     },
-
-    deleteItem(deleteItemId) {
-      axios.delete(`${API_ENDPOINT}/${deleteItemId}`)
-        .then(() => {
-          this.items = this.items.filter(item => item.id !== deleteItemId);
-        })
-        .catch((e) => {
-          this.errors.push(e);
+    computed: {
+      // a computed getter
+      completedCount() {
+        // `this` points to the vm instance
+        return this.items.filter(item => !item.isCompleted).length;
+      },
+      total() {
+        return this.items.length;
+      },
+      filteredItems() {
+        return this.items.filter((item) => {
+          switch (this.status) {
+            case 'active':
+              return !item.isCompleted;
+            case 'completed':
+              return item.isCompleted;
+            default:
+              return true;
+          }
         });
+      },
     },
-
-    deleteAllCompleted() {
-      const requests = this.items.filter(item => item.isCompleted).map((item) => {
-        return axios.delete(`${API_ENDPOINT}/${item.id}`)
-      });
-
-      Promise.all(requests)
-        .then(() => {
-          this.items = this.items.filter(item => !item.isCompleted);
-        })
-        .catch((e) => {
-          this.errors.push(e);
-        });
-    },
-
-    toggleCompleted(toggledItemId) {
-      const editItem = this.items.find(item => item.id === toggledItemId);
-
-      axios.put(`${API_ENDPOINT}/${toggledItemId}`, {
-        isCompleted: !editItem.isCompleted,
-      })
-        .then((response) => {
-          this.items = this.items.map((item) => {
-            if (item.id === toggledItemId) {
-              return response.data;
-            }
-            return item;
+    methods: {
+      addItem(task) {
+        axios.post(API_ENDPOINT, {
+          task,
+        }, { withCredentials: true })
+          .then((response) => {
+            this.items.push(response.data);
+          })
+          .catch((e) => {
+            this.errors.push(e);
           });
-        })
-        .catch((e) => {
-          this.errors.push(e);
-        });
-    },
+      },
 
-    toggleAll(markCompleted) {
-      const requests = this.items.map(item => {
-        return axios.put(`${API_ENDPOINT}/${item.id}`, {
+      deleteItem(deleteItemId) {
+        axios.delete(`${API_ENDPOINT}/${deleteItemId}`, { withCredentials: true })
+          .then(() => {
+            this.items = this.items.filter(item => item.id !== deleteItemId);
+          })
+          .catch((e) => {
+            this.errors.push(e);
+          });
+      },
+
+      deleteAllCompleted() {
+        const requests = this.items.filter(item => item.isCompleted)
+          .map(item => axios.delete(`${API_ENDPOINT}/${item.id}`, { withCredentials: true }));
+
+        Promise.all(requests)
+          .then(() => {
+            this.items = this.items.filter(item => !item.isCompleted);
+          })
+          .catch((e) => {
+            this.errors.push(e);
+          });
+      },
+
+      toggleCompleted(toggledItemId) {
+        const editItem = this.items.find(item => item.id === toggledItemId);
+
+        axios.put(`${API_ENDPOINT}/${toggledItemId}`, {
+          isCompleted: !editItem.isCompleted,
+        }, { withCredentials: true })
+          .then((response) => {
+            this.items = this.items.map((item) => {
+              if (item.id === toggledItemId) {
+                return response.data;
+              }
+              return item;
+            });
+          })
+          .catch((e) => {
+            this.errors.push(e);
+          });
+      },
+
+      toggleAll(markCompleted) {
+        const requests = this.items.map(item => axios.put(`${API_ENDPOINT}/${item.id}`, {
           isCompleted: markCompleted,
-        });
-      });
+        }, { withCredentials: true }));
 
-      Promise.all(requests)
-        .then(() => {
-          this.items = this.items.map(item => ({
-            ...item,
-            isCompleted: markCompleted,
-          }));
-        })
-        .catch((e) => {
-          this.errors.push(e);
-        });
-    },
-
-    updateTask(itemId, task) {
-      axios.put(`${API_ENDPOINT}/${itemId}`, {
-        task,
-      })
-        .then((response) => {
-          this.items = this.items.map((item) => {
-            if (item.id === itemId) {
-              return response.data;
-            }
-            return item;
+        Promise.all(requests)
+          .then(() => {
+            this.items = this.items.map(item => ({
+              ...item,
+              isCompleted: markCompleted,
+            }));
+          })
+          .catch((e) => {
+            this.errors.push(e);
           });
-        })
-        .catch((e) => {
-          this.errors.push(e);
-        });
+      },
+
+      updateTask(itemId, task) {
+        axios.put(`${API_ENDPOINT}/${itemId}`, {
+          task,
+        }, { withCredentials: true })
+          .then((response) => {
+            this.items = this.items.map((item) => {
+              if (item.id === itemId) {
+                return response.data;
+              }
+              return item;
+            });
+          })
+          .catch((e) => {
+            this.errors.push(e);
+          });
+      },
     },
-  },
-};
+  };
 </script>
 
 <style scoped>
